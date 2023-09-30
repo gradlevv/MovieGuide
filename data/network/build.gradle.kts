d85b7feb86b66f1fc67a70ee69156ffda5a8990b
Javad Sadati <j.sadatii@gmail.com>
2023-09-30 16:40:26 +0330

    add kotlinx-serialization-json dependency

--- data/network/build.gradle.kts
+++ data/network/build.gradle.kts
@@ -1,46 +1,48 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.com.android.library)
-    alias(libs.plugins.org.jetbrains.kotlin.android)
+  alias(libs.plugins.com.android.library)
+  alias(libs.plugins.org.jetbrains.kotlin.android)
+  id("kotlinx-serialization")
 }
 
 android {
-    namespace = "com.example.data.network"
-    compileSdk = 33
+  namespace = "com.example.data.network"
+  compileSdk = 33
 
-    defaultConfig {
-        minSdk = 27
+  defaultConfig {
+    minSdk = 27
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    consumerProguardFiles("consumer-rules.pro")
+  }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      proguardFiles(
+        getDefaultProguardFile("proguard-android-optimize.txt"),
+        "proguard-rules.pro",
+      )
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
+  kotlinOptions {
+    jvmTarget = "1.8"
+  }
 }
 
 dependencies {
 
-    implementation(libs.core.ktx)
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.retrofit)
+  implementation(libs.core.ktx)
+  implementation(libs.appcompat)
+  implementation(libs.material)
+  implementation(libs.retrofit)
+  implementation(libs.kotlinx.serialization.json)
 
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+  testImplementation(libs.junit)
+  androidTestImplementation(libs.androidx.test.ext.junit)
+  androidTestImplementation(libs.espresso.core)
 }
