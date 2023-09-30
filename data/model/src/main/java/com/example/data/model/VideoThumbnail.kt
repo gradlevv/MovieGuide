f92826639342566e6347e7ef1f1f2aa2f747f54e
Javad Sadati <j.sadatii@gmail.com>
2023-09-30 12:41:14 +0330

    add model module

---
+++ data/model/src/main/java/com/example/data/model/VideoThumbnail.kt
@@ -0,0 +1,13 @@
+package com.example.data.model
+
+data class VideoThumbnail(
+  val ids: VideoId,
+  val title: String,
+  val posterUrl: String,
+  val year: Int,
+)
+
+data class VideoId(
+  val traktId: Int?,
+  val tmdbId: Int?,
+)
