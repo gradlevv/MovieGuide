749a192de78c2eab2e829873394fb358b8cbf3af
Javad Sadati <j.sadatii@gmail.com>
2023-09-30 12:20:17 +0330

    add spotless

--- app/src/main/java/com/example/movieguide/MainActivity.kt
+++ app/src/main/java/com/example/movieguide/MainActivity.kt
@@ -13,34 +13,34 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.movieguide.ui.theme.MovieGuideTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            MovieGuideTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      MovieGuideTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+          modifier = Modifier.fillMaxSize(),
+          color = MaterialTheme.colorScheme.background,
+        ) {
+          Greeting("Android")
         }
+      }
     }
+  }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+  Text(
+    text = "Hello $name!",
+    modifier = modifier,
+  )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MovieGuideTheme {
-        Greeting("Android")
-    }
+  MovieGuideTheme {
+    Greeting("Android")
+  }
 }
