6634f0b496b130b459923a6e9310dfc9a3977802
Javad Sadati <j.sadatii@gmail.com>
2023-10-04 11:58:32 +0330

    add kotlinx serialization converter

--- data/network/src/main/java/com/example/data/network/TmdbMovieService.kt
+++ data/network/src/main/java/com/example/data/network/TmdbMovieService.kt
@@ -1,9 +1,10 @@
 package com.example.data.network
 
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface TmdbMovieService {
 
   @GET("/movie/{movie_id}")
-  suspend fun getMovieDetails(movieId: Int): TmdbDetailResponse
+  suspend fun getMovieDetails(@Path("movie_id") movieId: Int): TmdbDetailResponse
 }
