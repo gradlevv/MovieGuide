5bd520ed3c22aa6d9b47abcacde504124b1792c2
Javad Sadati <j.sadatii@gmail.com>
2023-12-24 15:10:30 +0330

    add trending movie feature

---
+++ feature/home/build.gradle.kts
@@ -0,0 +1,71 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+  alias(libs.plugins.com.android.library)
+  alias(libs.plugins.org.jetbrains.kotlin.android)
+
+  kotlin("kapt")
+  alias(libs.plugins.hilt.android)
+}
+
+android {
+  namespace = "com.example.feature.movie_detail"
+  compileSdk = 33
+
+  defaultConfig {
+    minSdk = 24
+
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    consumerProguardFiles("consumer-rules.pro")
+  }
+
+  buildFeatures {
+    compose = true
+  }
+
+  composeOptions {
+    kotlinCompilerExtensionVersion = "1.4.3"
+  }
+
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      proguardFiles(
+        getDefaultProguardFile("proguard-android-optimize.txt"),
+        "proguard-rules.pro",
+      )
+    }
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
+  kotlinOptions {
+    jvmTarget = "17"
+  }
+}
+
+dependencies {
+
+  implementation(project(":domain:tmdb-movies"))
+  implementation(project(":data:model"))
+  implementation(libs.core.ktx)
+  implementation(libs.appcompat)
+  implementation(libs.material)
+
+  implementation(platform(libs.compose.bom))
+  implementation(libs.ui)
+  implementation(libs.ui.graphics)
+  implementation(libs.ui.tooling.preview)
+  implementation(libs.material3)
+  implementation(libs.lifecycle.viewmodel.compose)
+  implementation(libs.lifecycle.runtime.compose)
+
+  implementation(libs.android.hilt)
+  kapt(libs.android.hilt.compiler)
+
+  implementation(libs.coil.compose)
+
+  testImplementation(libs.junit)
+  androidTestImplementation(libs.androidx.test.ext.junit)
+  androidTestImplementation(libs.espresso.core)
+}
